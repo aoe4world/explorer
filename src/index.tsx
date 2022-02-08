d86a5a872ee2961f83d85e72d46c4b9a73cba331
Robert van Hoesel <robert@crowded.co>
2022-02-08 06:28:10 +0100

    Add navigation callback for embedding, fix prod

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,31 @@ import "./index.css";
 import { render } from "solid-js/web";
 import { Router } from "solid-app-router";
 
-import App from "./App";
+import App, { activePage } from "./App";
+import { createEffect, on } from "solid-js";
 interface explorerOptions {
   /** The path at which the explorer is located */
   base?: string;
+  /** Callback function to be used after each navigation */
+  onNavigate?: (page: { title?: string; description?: string }) => void;
 }
 
 export function initializeExplorer(el: HTMLElement = document.getElementById("explorer"), options: explorerOptions = {}) {
+  let router: typeof Router;
   render(
     () => (
-      <Router base={options.base}>
+      <Router base={options.base} ref={router}>
         <App />
       </Router>
     ),
     el
   );
+
+  if (options.onNavigate) {
+    createEffect(
+      on(activePage, () => {
+        options.onNavigate({ title: activePage().title, description: activePage().description });
+      })
+    );
+  }
 }
