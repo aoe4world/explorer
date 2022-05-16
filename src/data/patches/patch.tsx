df0e0b67966148c0ccfeeecb1b688b4749f3d3b0
Robert van Hoesel <robert@crowded.co>
2022-05-16 23:08:31 +0200

    Fix hre gold and date

--- src/data/patches/patch.tsx
+++ src/data/patches/patch.tsx
@@ -1534,7 +1534,7 @@ const serverSidePatch14681: PatchNotes = {
   name: "Server-Side Patch 14681 ",
   officialUrl: "https://www.ageofempires.com/news/age-of-empires-iv-server-side-patch-14681/",
   summary: `"In Server-Side Patch 14681, we’re bringing you changes to Mangonels! We recognize there’s been a lot of discussion around Mangonels, and we believe these changes build upon the unit’s uniqueness. We’re always open to more feedback, however! You may also be excited to know that the HRE’s Prelates have remembered how to shuffle their feet and no longer get stuck when inspiring their allies. "`,
-  date: new Date("2022-05-16"),
+  date: new Date("2022-05-16T20:00:00.000Z"),
   sections: [
     {
       title: "Balance Changes",
@@ -1573,7 +1573,7 @@ const serverSidePatch14681: PatchNotes = {
         {
           items: [],
           civs: ["hr"],
-          diff: [["nerf", "HRE will now start the game with 100 gold (down from 200 gold)"]],
+          diff: [["nerf", "HRE will now start the game with 0 gold (down from 100 gold)"]],
           note: "We did some modeling and found the HRE generate less resources than other civilizations, leading to a weaker Feudal Age and less options. Removing the starting gold ensures they don't have a bonkers Feudal Age speed, and now bolster a competitive number of resources in Feudal age.",
         },
         {
