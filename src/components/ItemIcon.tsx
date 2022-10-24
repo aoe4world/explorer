0f7e625a9f5bce2263afe4cc90e8f43d97b4ea5f
Robert van Hoesel <robert@crowded.co>
2022-10-24 22:00:57 +0200

    Remove local image url rewrites in hope of fixing things

--- src/components/ItemIcon.tsx
+++ src/components/ItemIcon.tsx
@@ -2,8 +2,5 @@ import { Component, JSX, splitProps } from "solid-js";
 
 export const ItemIcon: Component<JSX.ImgHTMLAttributes<any> & { url: string }> = (props) => {
   const [local, imgProps] = splitProps(props, ["url"]);
-
-  const url = () =>
-    import.meta.env.IMG_BASE_URL ? local.url?.replace("https://data.aoe4world.com/images/", import.meta.env.IMG_BASE_URL as string) : local.url;
-  return <img src={url()} {...imgProps} />;
+  return <img src={local.url} {...imgProps} />;
 };
