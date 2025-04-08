6c36256082988d9cb1fb786a69e8232a94e34ae7
Taloth Saldono <Taloth@users.noreply.github.com>
2025-04-08 04:08:28 +0200

    Small tweaks for cloudflare pages

--- src/components/ItemIcon.tsx
+++ src/components/ItemIcon.tsx
@@ -2,7 +2,10 @@ import { Component, JSX, splitProps } from "solid-js";
 
 export const ItemIcon: Component<JSX.ImgHTMLAttributes<any> & { url: string }> = (props) => {
   const [local, imgProps] = splitProps(props, ["url"]);
+  
+  const useLocal = (import.meta.env.DEV || window.location.hostname.endsWith('pages.dev'));
+  const inRuby = !!import.meta.env.VITE_RUBY_SOURCE_CODE_DIR;
+  const url = useLocal ? local.url?.replace(/https?:\/\/[a-z0-9.]+\/images/, inRuby ? '/vite-dev/lib/explorer/data/images' : '') : local.url;
 
-  const url = import.meta.env.DEV ? local.url?.replace(/https?:\/\/[a-z0-9.]+\/images/, '') : local.url;
   return <img src={url} {...imgProps} />;
 };
