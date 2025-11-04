e822c74f262aa3bd50bd27e55c51f75f34ae0741
Taloth Saldono <Taloth@users.noreply.github.com>
2025-11-05 00:07:44 +0100

    And fixed flags

--- src/components/CivFlag.tsx
+++ src/components/CivFlag.tsx
@@ -1,8 +1,10 @@
 import { Component, ComponentProps, splitProps } from "solid-js";
+import { CIVILIZATIONS } from "../config";
 import { FLAGS } from "../../assets";
 import { civAbbr } from "../types/data";
 
 export const CivFlag: Component<ComponentProps<"img"> & { abbr: civAbbr }> = (props) => {
   const [civ, rest] = splitProps(props, ["abbr"]);
-  return <img src={FLAGS[civ.abbr]} {...rest} />;
+  const slug = CIVILIZATIONS[civ.abbr].slug;
+  return <img src={FLAGS[slug]} {...rest} />;
 };
